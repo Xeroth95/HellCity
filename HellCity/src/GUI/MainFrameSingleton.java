42ccde9225683b7835a583ff9b4f71d771949d58
Sebastian Sura <s.sura@hotmail.de>
2014-02-16 00:30:22 +0100

    Can almost be started!
    Signed-off-by: Sebastian Sura <s.sura@hotmail.de>

--- HellCity/src/GUI/MainFrameSingleton.java
+++ HellCity/src/GUI/MainFrameSingleton.java
@@ -1,5 +1,6 @@
 package GUI;
 
+import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 public class MainFrameSingleton {
@@ -16,11 +17,28 @@ public class MainFrameSingleton {
 		}
 	}
 	
-	public static class MainFrame {
+	public static class MainFrame extends JFrame {
 		JPanel contentPane;
 		
 		private MainFrame() {
 			// TODO: implement the main frame
+			super("HellCity v0.0.1");
+			this.initComponents();
+			this.pack();
+		}
+		
+		@Override
+		public void setVisible(boolean b) {
+			// TODO: check for validity
+			assert (contentPane != null) : "Fehler beim Initialisieren der MainFrame (InitError: contentPane)";
+			super.setVisible(b);
+		}
+		
+		private void initComponents() {
+			this.contentPane = new JPanel();
+			this.contentPane.setPreferredSize(new java.awt.Dimension(640, 480));
+			this.setContentPane(this.contentPane);
+			this.contentPane.setBackground(new java.awt.Color(255, 255, 255));
 		}
 	}
 }
--- HellCity/src/start/start.java
+++ HellCity/src/start/start.java
@@ -1,7 +1,12 @@
 package start;
 
+import javax.swing.JFrame;
+
+import GUI.MainFrameSingleton;
+
 public class start {
 	public static void main(String[] args) {
+		JFrame mainFrame = MainFrameSingleton.getFrame();
 		System.out.println("Start the game!");
 	}
 }
