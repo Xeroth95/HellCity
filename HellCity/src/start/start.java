9c3b4f886a6e3f351a6795f6aa1e27daa5dea5bd
Hilwan Xetmen <shillah@hotmail.de>
2014-02-16 13:34:59 +0100

    Test commit 3

--- HellCity/src/start/start.java
+++ HellCity/src/start/start.java
@@ -8,6 +8,6 @@ public class start {
 	public static void main(String[] args) {
 		JFrame mainFrame = MainFrameSingleton.getFrame();
 		mainFrame.setVisible(true);
-		System.out.println("Start the game!");
+		System.out.println("Start the game, please ?!");
 	}
 }
